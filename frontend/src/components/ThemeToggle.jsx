8d1a98579cb2863e48b98b0204a4fd435fc27535
Soumyadeep paul <spaulisback@gmail.com>
2025-09-14 13:39:34 +0530

    done

--- frontend/src/components/ThemeToggle.jsx
+++ frontend/src/components/ThemeToggle.jsx
@@ -3,7 +3,10 @@ const ThemeToggle = ({ toggleTheme }) => {
     <div className="fixed top-4 right-4 z-50">
       <button
         onClick={toggleTheme}
-        className="bg-red-600 text-white px-4 py-2 rounded-xl hover:bg-red-700 transition text-sm font-semibold dark:bg-black dark:text-red-500 border border-white dark:border-red-500"
+        className="bg-red-600 text-white px-3 py-1 sm:px-4 sm:py-2 
+               rounded-xl hover:bg-red-700 transition text-xs sm:text-sm 
+               font-semibold dark:bg-black dark:text-red-500 
+               border border-white dark:border-red-500"
       >
         Toggle Theme
       </button>
